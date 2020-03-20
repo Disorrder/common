9782ad6cdfd9a82bd2376be05ff7d21f9b333d76
Alex <disorrder@gmail.com>
2020-03-20 11:04:02 +0300

    Update NetLoader.js

--- async/NetLoader.js
+++ async/NetLoader.js
@@ -1,71 +1,125 @@
-import ExposedPromise from "./ExposedPromise";
+import EventEmitter from "events";
+import createExposedPromise from "./ExposedPromise";
 
-// WIP type Item = {url, params} for fetch()?
+// interface IRequestItem = {id, request, data?, error?, retries?}
 
-export class NetLoader {
+export default class NetLoader extends EventEmitter {
     constructor() {
-        this.items = []; // store all items
-        this.pending = []; // store only items didn't start yet. Think about name
+        super();
+
+        this.maxThreads = 4;
+        this.maxRetry = 2; // Not implemented!
+
+        this.clear();
+    }
+
+    clear() {
+        this._idCounter = 0; // private
+        this.requests = {}; // store all Requests by id
+        this.queue = []; // store only items didn't start yet.
         
-        this.queue = []; // array for simultaneous requests
+        this.pending = []; // array for simultaneous requests
         this.fulfilled = [];
         this.rejected = [];
 
-        this.maxThreads = 4;
-        this.maxRetry = 3; // Not implemented!
-        
-        this.promise = new ExposedPromise();
+        this.promise = createExposedPromise();
     }
 
-    get complete() { return this.fulfilled.length + this.rejected.length; }
-    get total() { return this.items.length; }
-    get inProgress() { return this.total - this.complete; }
-    get progress() { return this.complete / this.total || 0; }
+    get loaded() { return this.fulfilled.length + this.rejected.length; }
+    get total() { return this._idCounter; }
+    get inProgress() { return this.total - this.loaded; }
+    get progress() { return this.loaded / this.total || 0; }
     get progressPerc() { return Math.round(this.progress * 1000) / 10 + "%"; }
 
-    add(item) {
-        this.items.push(item);
-        this.pending.push(item);
+    get status() {
+        return {
+            status: this.promise.status,
+            loaded: this.loaded,
+            total: this.total,
+            progress: this.progress,
+            progressPerc: this.progressPerc,
+        }
+    }
+
+    // type <Request>, support iOS >=10.1
+    add(request) {
+        if (this.promise.status > 0) this.clear();
+
+        const id = ++this._idCounter;
+        this.requests[id] = request;
+
+        const item = {
+            id,
+            request: request.clone(),
+            data: null,
+            error: null,
+        };
+
+        this.queue.push(item);
         this.tryNext();
+        return item;
     }
 
     async tryNext() {
-        if (!this.pending.length) return;
-        if (this.queue.length >= this.maxThreads) return;
+        if (this.promise.status > 0) return;
+        if (!this.queue.length) return;
+        if (this.pending.length >= this.maxThreads) return;
 
-        let item = this.pending.splice(0, 1)[0];
+        let item = this.queue.shift();
         let action = this.action(item);
-        this.queue.push(action);
+        this.pending.push(item);
+        this.emit("progress", this.progress)
 
         try {
             let {data} = await action;
-            item.__data = data;
+            item.data = data;
             this.fulfilled.push(item);
         } catch (error) {
-            item.__error = error;
+            item.error = error;
             // TODO: add retry
+            this.pending.splice(this.pending.indexOf(action), 1);
             this.rejected.push(item);
+            await this.promise.reject(error);            
+            this.emit("error", error);
+            return;
         }
+        
+        this.pending.splice(this.pending.indexOf(action), 1);
+        this.emit("progress", this.progress)
 
-        this.queue.splice(this.queue.indexOf(action), 1);
-
-        if (this.complete >= this.total) {
-            this.promise.resolve();
+        if (this.loaded >= this.total) {
+            await this.promise.resolve();
+            this.emit("finish");
         } else {
             this.tryNext();
         }
     }
 
-    async action({url, params}) {
-        // overload it
-        // let res = fetch(url, params);
-        console.log("def action", url, params);
-        let to = Math.random()*2000;
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                console.log("TO", to);
-                resolve({data: "ok"});
-            }, to);
+    async action(item) {
+        return fetch(item.request);
+    }
+
+    async retryAll() {
+        const pending = this.pending.map((item) => {
+            return {
+                ...item,
+                request: this.requests[item.id].clone()
+            }
         });
+        const rejected = this.rejected.map((item) => {
+            return {
+                ...item,
+                request: this.requests[item.id].clone()
+            };
+        });
+        
+        this.queue = [...this.queue, ...pending, ...rejected];
+        this.pending = [];
+        this.rejected = [];
+        this.promise = createExposedPromise();
+
+        for(let i = 0; i < this.maxThreads; i++) {
+            this.tryNext();
+        }
     }
 }
